7fe2728b977aebf1b9c7d775228329a4d72a2e12
ByunJungWon <jungwon2580147@gmail.com>
2026-06-07 21:34:39 +0900

    날짜 표시 유틸리티 추가

---
+++ Wakey/Wakey/Utilities/Date+Wakey.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+extension Date {
+    static func alarmTime(hour: Int, minute: Int) -> Date {
+        Calendar.current.date(bySettingHour: hour, minute: minute, second: 0, of: Date()) ?? Date()
+    }
+
+    var alarmTimeText: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: self)
+    }
+
+    var koreanMonthDayText: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "M월 d일"
+        return formatter.string(from: self)
+    }
+
+    var koreanFullDateText: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "M월 d일 EEEE"
+        return formatter.string(from: self)
+    }
+
+    var koreanGeneratedText: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월 d일 a h:mm"
+        return formatter.string(from: self)
+    }
+}
